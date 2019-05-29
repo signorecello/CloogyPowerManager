3c87ee5b9694b42eeefe4efdd4b2521f7921dd23
José Pedro Sousa <jpedrosous@gmail.com>
2019-05-29 20:49:44 +0100

    trying to add power get endpoint

--- index.js
+++ index.js
@@ -11,25 +11,30 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get("/", function(req, res) {
-    res.json("Power manager starting (check console).");
-    //powermanager.powerManager('firstBoot')
-    res.end();
+	res.json("Power manager starting (check console).");
+	//powermanager.powerManager('firstBoot')
+	res.end();
 });
 
 app.post("/actuate", async function(req, res) {
-    pass = req.body.weakPass;
-    if (pass === process.env.REMOTE_ACTUATION_PASS) {
-        let token = await powermanager.grabToken();
-        await actuators.actuate(token, 3);
-        res.json("Plug actuated!");
-        res.end();
-    }
+	pass = req.body.weakPass;
+	if (pass === process.env.REMOTE_ACTUATION_PASS) {
+		let token = await powermanager.grabToken();
+		await actuators.actuate(token, 3);
+		res.json("Plug actuated!");
+		res.end();
+	}
+});
+
+app.get("/current_usage", async function(req, res) {
+	res.json(powermanager.averageUnit);
+	res.end();
 });
 
 setInterval(function() {
-    http.get("http://cloogypm.herokuapp.com/");
+	http.get("http://cloogypm.herokuapp.com/");
 }, 300000); // every 5 minutes (300000)
 
 app.listen(port, function() {
-    console.log(chalk.cyan(`Listening on port ${port}`));
+	console.log(chalk.cyan(`Listening on port ${port}`));
 });
